ae65c8f49ce695acd10c76cc584d7c626e690a9a
caiocaldeira3 <caio.caldeira3@gmail.com>
2024-02-02 10:32:14 -0300

    Improve main script for testing

--- app/main.py
+++ app/main.py
@@ -1,11 +1,5 @@
-from collections.abc import Iterable
+from app.parsing.methods import parse_expense
+from app.user.methods import ensure_profile
 
-from app.parsing.methods import (discriminate_expenses, get_file_ocr,
-                                 get_pdf_file)
-
-
-def parse_expense (file_name: str, groups: Iterable[str]) -> None:
-    file_content = get_pdf_file(file_name)
-    file_ocr = get_file_ocr(file_content)
-
-    return discriminate_expenses(file_ocr, groups)
+user = ensure_profile()
+exp = parse_expense("primeira_conta.jpeg", user)
